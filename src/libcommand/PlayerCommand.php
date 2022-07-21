0fe1eb5cddfd450c318f9acc778b887649141710
Matthew Jordan <mattjordanbusiness@gmail.com>
2022-07-21 17:59:44 -0500

    `PlayerCommand`: Use `$sender` instead of `$player`

--- src/libcommand/PlayerCommand.php
+++ src/libcommand/PlayerCommand.php
@@ -37,9 +37,9 @@ abstract class PlayerCommand extends Command {
 	/**
 	 * The method that is called when the command is executed by a player.
 	 *
-	 * @param Player $player
+	 * @param Player $sender
 	 * @param array<string, mixed> $arguments
 	 * @return bool|string
 	 */
-	public abstract function onPlayerExecute(Player $player, array $arguments): bool|string;
+	public abstract function onPlayerExecute(Player $sender, array $arguments): bool|string;
 }
