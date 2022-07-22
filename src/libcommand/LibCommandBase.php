38b07738ff8060ddd1d0396d51d0eef0d0686b85
Matthew Jordan <mattjordanbusiness@gmail.com>
2022-07-22 18:31:19 -0500

    `LibCommandBase`: Set static properties as private

--- src/libcommand/LibCommandBase.php
+++ src/libcommand/LibCommandBase.php
@@ -33,9 +33,9 @@ use function array_map;
 final class LibCommandBase {
 
 	/** @var array<Command> */
-	public static array $commands;
+	private static array $commands;
 
-	public static bool $registered = false;
+	private static bool $registered = false;
 
 	public static function register(PluginBase $plugin): void {
 		if (self::$registered) {
